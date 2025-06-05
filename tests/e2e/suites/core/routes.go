e76d4835db8af4047e8fbe25b3ddb328e197e3b1
Milind Yadav <miyadav@redhat.com>
2025-06-05 12:53:32 +0530

    route controller test for zone/regions

--- tests/e2e/suites/core/routes.go
+++ tests/e2e/suites/core/routes.go
@@ -2,14 +2,16 @@ package core
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/miyadav/ccm-conformance-tests/tests/e2e/framework"
+
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = ginkgo.Describe("CCM Routes Management", func() {
+var _ = ginkgo.Describe("CCM Routes controller", func() {
 	var f *framework.Framework
 
 	ginkgo.BeforeEach(func() {
@@ -30,5 +32,19 @@ var _ = ginkgo.Describe("CCM Routes Management", func() {
 			}
 		})
 
+	})
+
+	var _ = ginkgo.Describe("Route/Zones Controller", func() {
+		ginkgo.It("should get zone/region labels from all nodes", func() {
+			nodes, err := f.ClientSet.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Expect(len(nodes.Items)).To(gomega.BeNumerically(">", 0))
+			for _, node := range nodes.Items {
+				zone := node.Labels["topology.kubernetes.io/zone"]
+				region := node.Labels["topology.kubernetes.io/region"]
+				gomega.Expect(zone).NotTo(gomega.BeEmpty(), fmt.Sprintf("Node %s missing zone label", node.Name))
+				gomega.Expect(region).NotTo(gomega.BeEmpty(), fmt.Sprintf("Node %s missing region label", node.Name))
+			}
+		})
 	})
 })
